79756866c60e8fabf049f4d7e98db56521d8baa1
vicziani <viczian.istvan@gmail.com>
2024-07-31 13:39:55 +0200

    Integrációs tesztek

--- employees-repo/src/test/java/employees/EmployeesResourceTest.java
+++ employees-repo/src/test/java/employees/EmployeesResourceTest.java
@@ -1,16 +1,33 @@
 package employees;
 
+import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import training.dto.EmployeeDto;
+
+import java.util.List;
+import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @QuarkusTest
 class EmployeesResourceTest {
+
+    @Inject
+    EmployeesRepository employeesRepository;
+
+//    @BeforeEach
+//    @Transactional
+//    void emptyTables() {
+//        employeesRepository.getEntityManager().createNativeQuery("delete from employee").executeUpdate();
+//    }
 
     @Test
     void createEmployee() {
@@ -24,5 +41,35 @@ class EmployeesResourceTest {
 
         assertThat(result.getName())
                 .startsWith("Jack");
+    }
+
+    @Test
+    void listEmployees() {
+        var name = "John Doe " + UUID.randomUUID();
+        Log.infof("Name: %s", name);
+
+        createTestEmployee(name);
+
+//        given()
+//                .contentType(ContentType.JSON)
+//                .body(new EmployeeDto().name("Jack Doe"))
+//                .when().post("/api/employees")
+//                .then()
+//                .statusCode(Response.Status.CREATED.getStatusCode());
+
+        var employees = given()
+                .when().get("/api/employees")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().as(new TypeRef<List<EmployeeDto>>(){});
+
+        assertThat(employees)
+                .extracting(EmployeeDto::getName)
+                .contains(name);
+    }
+
+    @Transactional
+    void createTestEmployee(String name) {
+        employeesRepository.persist(new Employee(name));
     }
 }
